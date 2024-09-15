f05f9faa3c994270f712ab19028959eb1097664a
haoyang9804 <haoyanghkust@gmail.com>
2024-09-15 15:43:15 +0100

    add test_validity.py

---
+++ test_validity.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+
+def test_validity():
+  if not os.path.exists('generated_programs'):
+    return
+  for file in os.listdir('generated_programs'):
+    file_path = os.path.join('./generated_programs', file)
+    p = subprocess.run(f'solc {file_path}', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if p.returncode:
+      print(f'Error in file {file_path}')
+      print(p.stderr.decode('utf-8'))
+  
+test_validity()
