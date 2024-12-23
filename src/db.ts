1cc2ebf37d2df36c127aeed084b1eb4239a75afc
Haoyang Ma <haoyanghkust@gmail.com>
2024-12-23 23:29:34 +0000

    remove unrelated packages

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,3 @@
-import * as sqlite from 'sqlite';
-import * as sqlite3 from 'sqlite3';
 import { Tree } from './dataStructor';
 import { get_scope_from_scope_id, inside_contract, scopeKind, ScopeList } from './scope';
 import { assert } from './utility';
@@ -11,47 +9,6 @@ import { IRExpressionStatement, IRStatement } from './statement';
 import { new_global_id } from './genContext';
 import { IRAssignment, IRIdentifier } from './expression';
 import { cur_scope } from './genContext';
-
-//@ts-ignore
-// Deprecated Database
-class DeprecatedDB {
-  db : sqlite.Database;
-  constructor(filename : string = ":memory:") {
-    const config = {
-      filename: filename,
-      driver: sqlite3.Database
-    };
-    this.db = new sqlite.Database(config);
-  }
-
-  async open() {
-    await this.db.open();
-  }
-
-  async init() {
-    await this.db.exec('CREATE TABLE tbl (id INTEGER PRIMARY KEY, scope INTEGER, kind TEXT, type TEXT)')
-  }
-
-  async close() {
-    await this.db.close();
-  }
-
-  async run(cmd : string) : Promise<void | any[]> {
-    if (cmd.startsWith("INSERT") || cmd.startsWith("UPDATE")) {
-      await this.db.run(cmd);
-    }
-    else if (cmd.startsWith("SELECT")) {
-      const result = await this.db.all(cmd) as any[];
-      return result;
-    }
-  }
-
-  async insert(id : number, scope : number, kind : string) {
-    const cmd = `INSERT INTO tbl (id, scope, kind) VALUES (${id}, ${scope}, "${kind}")`;
-    await this.run(cmd);
-  }
-}
-
 /**
  * A class to store the declaration information.
  */
